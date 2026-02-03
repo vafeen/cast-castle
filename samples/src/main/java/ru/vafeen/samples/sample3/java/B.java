0017520da110b42b33846b9914d4d77ab36ad9d1
A <666av6@gmail.com>
2026-02-03 21:38:09 +0300

    add sample on java

---
+++ samples/src/main/java/ru/vafeen/samples/sample3/java/B.java
@@ -0,0 +1,27 @@
+package ru.vafeen.samples.sample3.java;
+
+public class B {
+    private int x;
+    private int z;
+
+    public B(int x, int z) {
+        this.x = x;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+}
